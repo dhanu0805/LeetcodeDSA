ee8b390f8061eddbddd092c110c4afd072694521
dhanu0805 <22a91a0571@aec.edu.in>
2025-10-28 18:48:41 +0530

    Time: 1 ms (83.13%) | Memory: 42.4 MB (42.17%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int n=nums.length;
+        int prefix[]=new int[n];
+        int sufix[]=new int[n];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i-1];
+        }
+         for(int i=n-2;i>=0;i--){
+            sufix[i]=sufix[i+1]+nums[i+1];
+        }
+        int ans=0;
+         for(int i=0;i<n;i++){
+            if(nums[i]==0){
+              if(prefix[i]==sufix[i]) ans+=2;
+              else if(Math.abs(prefix[i]-sufix[i])==1)ans+=1;
+            }
+        }
+        return ans;
+    }
+}
