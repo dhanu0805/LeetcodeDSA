e4dfd39706c2365d6f5b0d45eb085b9d9881c9e9
dhanu0805 <22a91a0571@aec.edu.in>
2026-01-04 11:50:52 +0530

    Time: 29 ms (45.20%) | Memory: 46.5 MB (33.08%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -4,7 +4,7 @@ class Solution {
         for(int num:nums){
             int sum=1+num;
             int c=0;
-            for(int i=2;i<=Math.sqrt(num);i++){
+            for(int i=2;i*i<=num;i++){
                   if(num%i==0){
                     int other=num/i;
                     if(i==other){
